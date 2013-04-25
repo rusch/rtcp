6dda576a491057aea07008d09f3906e006552820
Christian Rusch <christian.rusch@wingo.ch>
2013-04-25 10:32:58 +0200

    Improve documentation of the RTCP class

--- lib/rtcp.rb
+++ lib/rtcp.rb
@@ -21,7 +21,7 @@ class RTCP
     end
   end
 
-  # Decodes only the first RTCP packet and returns it
+  # Decodes the supplied RTCP packet and returns it
   def self.decode(data)
     raise(RTCP::DecodeError, "Truncated Packet") if (data.length < 4)
 
@@ -32,7 +32,7 @@ class RTCP
     self.packet_class(packet_type).new.decode(data.slice(0..(length - 1)))
   end
 
-  # Decodes all RTCP packets and returns them in an array
+  # Decodes all RTCP packets in the supplied string returns them in an array
   def self.decode_all(data)
     packets = []
     while data && data.length > 0
@@ -51,22 +51,35 @@ class RTCP
     self
   end
 
+  # Returns the packet as RTCP data string
   def to_s
     @packet_data
   end
 
   protected
 
+  # Ensures that the current RTCP Packet object is able to decode the RTCP
+  # packet with the given Packet Type ID.
+  #
+  # Raises an RTCP::DecodeError exception when this is not the case.
   def ensure_packet_type(packet_type)
     if packet_type != self.class::PT_ID
       raise(RTCP::DecodeError, "Wrong Packet Type. packet_type=#{packet_type}")
     end
   end
 
-  def payload_data(packet_data, length, header_length)
-    if packet_data.length > length
-      @packet_data = packet_data[0..length]
-    elsif packet_data.length == length
+  # Extracts and returns the payload data from the given packet_data using the
+  # supplied packet length and header_length values.
+  #
+  # It also sets the @packet_data instance variable, which is currently used
+  # by the to_s method for returning the packet data.
+  #
+  # Raises an RTCP::DecodeError exception when the packet_data is shorter
+  # than packet_length.
+  def payload_data(packet_data, packet_length, header_length)
+    if packet_data.length > packet_length
+      @packet_data = packet_data[0..packet_length]
+    elsif packet_data.length == packet_length
       @packet_data = packet_data
     else
       raise RTCP::DecodeError, "Truncated Packet"
@@ -77,6 +90,8 @@ class RTCP
 
   private
 
+  # Returns the Class to use for handling RTCP packets of the given packet
+  # type.
   def self.packet_class(packet_type)
     @@packet_classes[packet_type] || self
   end
